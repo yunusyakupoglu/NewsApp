060ccb63ff1e50279ea3b9be8365ce70efece9d5
Yunus Bayram YAKUPOĞLU <yunuscakal60@gmail.com>
2022-04-07 01:33:51 +0300

    Login activity finished.

--- app/src/main/java/com/news/newsapp/LoginActivity.java
+++ app/src/main/java/com/news/newsapp/LoginActivity.java
@@ -2,13 +2,28 @@ package com.news.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabActivityLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fabActivityLogin = findViewById(R.id.fab_activity_login);
+        fabActivityLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toMainActivity = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(toMainActivity);
+            }
+        });
     }
 }
--- app/src/main/java/com/news/newsapp/MainActivity.java
+++ app/src/main/java/com/news/newsapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.news.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabActivityMain = findViewById(R.id.fab_activity_main);
+        fabActivityMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toLoginActivity = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(toLoginActivity);
+            }
+        });
     }
 }
